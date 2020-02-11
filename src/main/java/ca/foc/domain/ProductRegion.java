3b1d66750f1dfcb05052b850a5fad11dc11c662b
ClaudiaPRG <59589262+ClaudiaPRG@users.noreply.github.com>
2020-02-11 09:17:00 -0700

    .

--- src/main/java/ca/foc/domain/ProductRegion.java
+++ src/main/java/ca/foc/domain/ProductRegion.java
@@ -5,15 +5,18 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
+@IdClass(ProductRegion.class)
 @Table(name="foc_product_region")
 public class ProductRegion implements Serializable{
 	@Id
+//	private int prod_id;
 	@ManyToOne
-	@JoinColumn
+	@JoinColumn(name="prod_id")
 	private Product product;
 	@Id
+//	private int reg_id;
 	@ManyToOne
-	@JoinColumn
+	@JoinColumn (name="reg_id")
 	private Region region;
 	
 
